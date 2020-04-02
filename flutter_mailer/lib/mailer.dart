7f17612371921865a285bd70a07d1296c9a1d1c8
hungtp <hungtp.personal@gmail.com>
2020-04-02 17:35:46 +0700

    Add GetIt register

--- flutter_mailer/lib/mailer.dart
+++ flutter_mailer/lib/mailer.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
 
+import 'blocs/bloc_index.dart';
 import 'screens/screens_index.dart';
 
-class Mailer extends StatelessWidget {
+class Mailer extends StatefulWidget {
+  @override
+  _MailerState createState() => _MailerState();
+}
+
+class _MailerState extends State<Mailer> {
+  @override
+  void initState() {
+    super.initState();
+    registerGetIt();
+  }
+
+  void registerGetIt() {
+    final getIt = GetIt.instance;
+
+    getIt.registerFactory<HomeBloc>(() => HomeBloc());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
