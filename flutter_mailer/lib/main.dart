d6a6e1baebe982242badec3545c1893a0e607b45
hungtp <hungtp.personal@gmail.com>
2020-04-02 16:57:56 +0700

    Structure app

--- flutter_mailer/lib/main.dart
+++ flutter_mailer/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'mailer.dart';
+import 'package:flutter_fimber/flutter_fimber.dart';
 
 void main() {
+  // Setting for Fimber logging
+  Fimber.plantTree(FimberTree(useColors: true));
+  Fimber.plantTree(DebugTree(useColors: true));
+
+  // Setting for forcing portrait
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  // run app
   runApp(Mailer());
 }
