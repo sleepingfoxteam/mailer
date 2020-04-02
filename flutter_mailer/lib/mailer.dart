e58dcf74aebf0bd6f443cf67f8970ca97be3b390
hungtp <hungtp.personal@gmail.com>
2020-04-02 16:44:05 +0700

    Structure launcher

---
+++ flutter_mailer/lib/mailer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'screens/screens_index.dart';
+
+class Mailer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
