aff820db75a062f5027f2aca3747c2b0ecfb1dbb
hungtp <hungtp.personal@gmail.com>
2020-04-02 23:40:25 +0700

    Update UserRepo

--- flutter_mailer/lib/repositories/user_repo/user_repo.dart
+++ flutter_mailer/lib/repositories/user_repo/user_repo.dart
@@ -1,3 +1,4 @@
+import 'package:fluttermailer/repositories/network_exception.dart';
 import 'package:fluttermailer/repositories/repo_result.dart';
 import 'package:fluttermailer/services/service_index.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -9,5 +10,14 @@ class UserRepo {
   Future<RepoResult<GoogleSignInAccount>> signInWithGoogle() async {
     final result = await _userNetworkService.signInWithGoogle();
     return RepoResult.from(result);
+  }
+
+  Future<RepoResult<String>> checkUserLogin() async {
+    String accessToken = await _userLocalService.checkUserLogin();
+    if (accessToken != null && accessToken.isNotEmpty) {
+      return RepoResult.success(accessToken);
+    } else {
+      return RepoResult.error(NetworkException(message: "User is not log in"));
+    }
   }
 }
