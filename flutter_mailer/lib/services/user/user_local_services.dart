88753e092eccb1c8417c2180a039c56be70d56e8
hungtp <hungtp.personal@gmail.com>
2020-04-02 23:40:54 +0700

    Update UserLocalService

--- flutter_mailer/lib/services/user/user_local_services.dart
+++ flutter_mailer/lib/services/user/user_local_services.dart
@@ -1,1 +1,11 @@
-class UserLocalService {}
+import 'package:fluttermailer/utils/share_preferences_service.dart';
+import 'package:get_it/get_it.dart';
+
+class UserLocalService {
+  final _sharePreferencesService = GetIt.I<SharePreferenceService>();
+
+  Future<String> checkUserLogin() async {
+    String accessToken = await _sharePreferencesService.getUser();
+    return accessToken;
+  }
+}
