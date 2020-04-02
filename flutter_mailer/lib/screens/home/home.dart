20583d0db008a44949a9178bbd34f85a8b538ec5
hungtp <hungtp.personal@gmail.com>
2020-04-02 23:40:39 +0700

    Update Home

--- flutter_mailer/lib/screens/home/home.dart
+++ flutter_mailer/lib/screens/home/home.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttermailer/blocs/bloc_index.dart';
+import 'package:fluttermailer/screens/inbox/inbox.dart';
+import 'package:fluttermailer/utils/utils_index.dart';
 import 'package:get_it/get_it.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _bloc = GetIt.I<HomeBloc>();
+  final _navigator = GetIt.I<MailerNavigator>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate to Inbox if use had logged in
+    _bloc.streamAccessToken.listen((value) {
+      if (value != null && value.isNotEmpty) {
+        _navigator.openScreenAndRemoveOthers(Inbox());
+      }
+    });
+
+    // check user login
+    _bloc.checkUserLogin();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _bloc = GetIt.I<HomeBloc>();
     return Scaffold(
       body: Container(
         child: Center(
