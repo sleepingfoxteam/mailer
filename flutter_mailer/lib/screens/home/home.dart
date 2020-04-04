65d98d583d9104947d3822176b62ee0b36fb0a03
hungtp <hungtp.personal@gmail.com>
2020-04-04 11:29:42 +0700

    Update Home

--- flutter_mailer/lib/screens/home/home.dart
+++ flutter_mailer/lib/screens/home/home.dart
@@ -47,7 +47,7 @@ class _HomeState extends State<Home> {
                 .size
                 .height,
           ),
-          visible: value == false,
+          visible: value ?? true,
         );
       },
     );
