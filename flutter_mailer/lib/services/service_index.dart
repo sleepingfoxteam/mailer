b4f24fd27db51ae60526427945f5424995320246
hungtp <hungtp.personal@gmail.com>
2020-04-04 11:33:00 +0700

    Move network_exception

--- flutter_mailer/lib/services/service_index.dart
+++ flutter_mailer/lib/services/service_index.dart
@@ -1,2 +1,5 @@
+export 'base_api_service.dart';
+export 'base_network_object.dart';
+export 'network_exception.dart';
 export 'user/user_local_services.dart';
 export 'user/user_network_service.dart';
