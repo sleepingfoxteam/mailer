19ca0bea5ad17388f4e3bd63529fc09f29051394
hungtp <hungtp.personal@gmail.com>
2020-04-04 11:27:40 +0700

    Update InboxBloc

--- flutter_mailer/lib/blocs/inbox/inbox_bloc.dart
+++ flutter_mailer/lib/blocs/inbox/inbox_bloc.dart
@@ -1,3 +1,23 @@
 import 'package:fluttermailer/blocs/base_bloc.dart';
+import 'package:fluttermailer/models/model_index.dart';
+import 'package:fluttermailer/repositories/repository_index.dart';
+import 'package:get_it/get_it.dart';
 
-class InboxBloc extends BaseBloc {}
+class InboxBloc extends BaseBloc {
+  GmailRepo _gmailRepo;
+
+  InboxBloc({GmailRepo gmailRepo}) {
+    _gmailRepo = gmailRepo ?? GetIt.I<GmailRepo>();
+  }
+
+  void loadGmailUserProfile() async {
+    handleLoading(true);
+    RepoResult<GmailProfileModel> result = await _gmailRepo.getUserProfile(
+      uid: "me",
+    );
+    if (result.isSuccess()) {
+      print(result.data);
+    }
+    handleLoading(false);
+  }
+}
