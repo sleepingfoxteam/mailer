ab5ff1c1f2c1fb7d6115a1779e273a047c69ce52
hungtp <hungtp.personal@gmail.com>
2020-04-04 11:30:59 +0700

    Add GmailNetworkObject

--- flutter_mailer/lib/services/user/user_network_service.dart
+++ flutter_mailer/lib/services/user/user_network_service.dart
@@ -1,8 +1,19 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
+const GMAIL_SCOPES = [
+  "https://www.googleapis.com/auth/gmail.settings.sharing",
+  "https://www.googleapis.com/auth/gmail.settings.basic",
+  "https://www.googleapis.com/auth/gmail.metadata",
+  "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/gmail.compose",
+  "https://www.googleapis.com/auth/gmail.insert",
+];
+
 class UserNetworkService {
   Future<GoogleSignInAccount> signInWithGoogle() async {
-    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: GMAIL_SCOPES,
+    );
     GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
     return googleSignInAccount;
   }
