f5b7a1912f93c6e3b2f052fee6751d285ee17e66
hungtp <hungtp.personal@gmail.com>
2020-04-05 09:23:14 +0700

    Update Gmail scopes

--- flutter_mailer/lib/services/user/user_network_service.dart
+++ flutter_mailer/lib/services/user/user_network_service.dart
@@ -1,12 +1,7 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 const GMAIL_SCOPES = [
-  "https://www.googleapis.com/auth/gmail.settings.sharing",
-  "https://www.googleapis.com/auth/gmail.settings.basic",
-  "https://www.googleapis.com/auth/gmail.metadata",
-  "https://www.googleapis.com/auth/gmail.modify",
-  "https://www.googleapis.com/auth/gmail.compose",
-  "https://www.googleapis.com/auth/gmail.insert",
+  "https://mail.google.com/",
 ];
 
 class UserNetworkService {
