8420f29a4d5ca133765e9ded6031b5fec6733e1c
hungtp <hungtp.personal@gmail.com>
2020-04-05 17:18:23 +0700

    Update model_index

--- flutter_mailer/lib/models/model_index.dart
+++ flutter_mailer/lib/models/model_index.dart
@@ -2,3 +2,4 @@
 export 'gmail_models/gmail_message_result_model.dart';
 export 'gmail_models/gmail_profile_model.dart';
 export 'gmail_models/load_user_message_list_result_model.dart';
+export 'gmail_models/move_strash_result_model.dart';
