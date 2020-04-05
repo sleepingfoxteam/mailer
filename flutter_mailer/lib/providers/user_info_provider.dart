d894a2e34705c8fef052a860c9f0337b05c18a28
hungtp <hungtp.personal@gmail.com>
2020-04-05 09:20:56 +0700

    Update UserInfoProvider

--- flutter_mailer/lib/providers/user_info_provider.dart
+++ flutter_mailer/lib/providers/user_info_provider.dart
@@ -20,8 +20,6 @@ class UserInfoProvider {
   }) {
     googleSignInAccount = account;
     accessToken = authentication.accessToken;
-    print("++==++$accessToken");
     idToken = authentication.idToken;
-    print("++==++${account.id}");
   }
 }
