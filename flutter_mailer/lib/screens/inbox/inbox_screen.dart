1193b54b0c04305cc801b0d2982615604ddf0d91
hungtp <hungtp.personal@gmail.com>
2020-04-05 09:21:50 +0700

    Update InboxScreen

--- flutter_mailer/lib/screens/inbox/inbox_screen.dart
+++ flutter_mailer/lib/screens/inbox/inbox_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttermailer/blocs/bloc_index.dart';
 import 'package:fluttermailer/blocs/stream_data.dart';
+import 'package:fluttermailer/models/gmail_models/load_user_message_list_result_model.dart';
+import 'package:fluttermailer/providers/gmail_provider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 class InboxScreen extends StatefulWidget {
   @override
@@ -11,33 +14,58 @@ class InboxScreen extends StatefulWidget {
 
 class _InboxScreenState extends State<InboxScreen> {
   final _bloc = GetIt.I<InboxBloc>();
+  final _gmailProvider = GetIt.I<GmailProvider>();
 
   @override
   void initState() {
-    _bloc.loadGmailUserProfile();
+    checkMessage();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final mainOverlay = createStreamWidget(
-        streamData: _bloc.streamIsLoading,
-        buildChild: (context, value) {
-          return Visibility(
-            visible: value ?? true,
-            child: Container(
-              color: Colors.white,
-              child: Center(
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.red,
-                ),
+      streamData: _bloc.streamIsLoading,
+      buildChild: (context, value) {
+        return Visibility(
+          visible: value ?? true,
+          child: Container(
+            color: Colors.white,
+            child: Center(
+              child: createStreamWidget(
+                streamData: _bloc.streamLoadingPercent,
+                buildChild: (context, inValue) {
+                  return SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: LiquidCircularProgressIndicator(
+                      value: inValue ?? 0,
+                      valueColor:
+                      AlwaysStoppedAnimation(Colors.black.withOpacity(0.5)),
+                      backgroundColor: Colors.white,
+                      borderColor: Colors.transparent,
+                      borderWidth: 0.5,
+                    ),
+                  );
+                },
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
     final mainContent = Container(
       child: Center(
-        child: Text("Inbox screen"),
+        child: createStreamWidget(
+          streamData: _bloc.streamUserMessages,
+          buildChild: (context, value) {
+            return Text((value as LoadUserMessageListResultModel)
+                ?.messages
+                ?.length
+                ?.toString() ??
+                "Loading...");
+          },
+        ),
       ),
     );
     return Stack(
@@ -46,5 +74,21 @@ class _InboxScreenState extends State<InboxScreen> {
         mainContent,
       ],
     );
+  }
+
+  void checkMessage() {
+    if (_gmailProvider.loadUserMessageListResultModel == null ||
+        _gmailProvider.loadUserMessageListResultModel?.messages?.length == 0) {
+      _bloc.loadUserMessageList(
+        uid: "me",
+        includeSpamTrash: false,
+        maxResults: 5,
+        pageToken: "",
+        q: "",
+      );
+    }
+    else {
+      _bloc.getGmailMessageProvider();
+    }
   }
 }
